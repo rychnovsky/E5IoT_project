bf57a8398a1c52aca022f8b3a4050701656bb2bc
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2018-11-21 20:03:27 +0100

    Added button

--- button-client/button-client.ino
+++ button-client/button-client.ino
@@ -2,23 +2,52 @@
 
 GoogleMapsDeviceLocator locator;
 
+int ledPin = D1;
+
+int internalLedPin = D7;
+
+int buttonPin = D0;
 
 void setup() {
     Serial.begin(9600);
 
     Serial.println("Photon starting....");
     
+     // sets pin as input
+    pinMode( buttonPin , INPUT_PULLUP);
+
+    // sets pins as output
+    pinMode(ledPin, OUTPUT );
+    pinMode(internalLedPin, OUTPUT);
+    digitalWrite(ledPin, LOW);
+    digitalWrite(internalLedPin, LOW);
+    
     Particle.subscribe("distance_button_result_ack", onAckReceived, MY_DEVICES);
     
     
     // Scan for visible networks and publish to the cloud every 30 seconds
     // Pass the returned location to be handled by the locationCallback() method
-    locator.withSubscribe(onLocationReceived).withLocatePeriodic(20);
+    // locator.withSubscribe(onLocationReceived).withLocatePeriodic(60);
+    locator.withSubscribe(onLocationReceived).withLocateOnce();
 }
 
 void loop() {
+    // locator.loop();
+
     // todo call this only on button press
-    locator.loop();
+    int buttonState = digitalRead( buttonPin );
+
+    if ( buttonState == LOW )
+    {
+        Serial.println("Button pressed!");
+        digitalWrite( ledPin, HIGH);
+        delay(1500); // to prevent multiple 
+
+        locator.publishLocation();
+    } else {
+        digitalWrite( ledPin, LOW);
+    }
+    
 }
 
 /**
@@ -50,4 +79,9 @@ void onLocationReceived(float lat, float lon, float accuracy) {
 void onAckReceived(const char *event, const char *data) {
     Serial.println("Process finished, OK!");
     // TODO: indicate the result to user - eg blink a LED
+    digitalWrite(internalLedPin, HIGH);
+    delay(2000);
+    digitalWrite(internalLedPin, LOW);
+
 }
+
