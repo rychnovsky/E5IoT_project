f4f7325f5e77d485b9dc8155aae14e4b47c7b61c
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2018-12-06 18:55:37 +0100

    Swap A0-D0, A1-D1, A2-D2

--- button-master/button-master.ino
+++ button-master/button-master.ino
@@ -173,17 +173,17 @@ void showError() {
 * Switch off display
 */
 void resetDisplay(){
-    digitalWrite(D0,LOW);
-    digitalWrite(D1,LOW);
-    digitalWrite(D2,LOW);
+    digitalWrite(A0,LOW);
+    digitalWrite(A1,LOW);
+    digitalWrite(A2,LOW);
     digitalWrite(D3,LOW);
     digitalWrite(D4,LOW);
     digitalWrite(D5,LOW);
     digitalWrite(D6,LOW);
     digitalWrite(D7,LOW);
-    digitalWrite(A0,LOW);
-    digitalWrite(A1,LOW);
-    digitalWrite(A2,LOW);
+    digitalWrite(D0,LOW);
+    digitalWrite(D1,LOW);
+    digitalWrite(D2,LOW);
     digitalWrite(A3,LOW);
     digitalWrite(A4,LOW);
     digitalWrite(A5,LOW);
@@ -198,68 +198,68 @@ void firstDigit(int number) {
           digitalWrite(A5,HIGH);
           digitalWrite(A4,HIGH);
           digitalWrite(A3,HIGH);
-          digitalWrite(A2,HIGH);
-          digitalWrite(A1,HIGH);
-          digitalWrite(A0,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D0,HIGH);
           break;
         case 1:
           digitalWrite(A3,HIGH);
-          digitalWrite(A2,HIGH);
+          digitalWrite(D2,HIGH);
           break;
         case 2:
           digitalWrite(D3,HIGH);
-          digitalWrite(A1,HIGH);
-          digitalWrite(A2,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D2,HIGH);
           digitalWrite(A4,HIGH);
           digitalWrite(A5,HIGH);
           break;
         case 3:
           digitalWrite(D3,HIGH);
           digitalWrite(A3,HIGH);
-          digitalWrite(A1,HIGH);
-          digitalWrite(A2,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D2,HIGH);
           digitalWrite(A4,HIGH);
           break;
         case 4:
           digitalWrite(D3,HIGH);
           digitalWrite(A3,HIGH);
-          digitalWrite(A2,HIGH);
-          digitalWrite(A0,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(D0,HIGH);
           break;
         case 5:
           digitalWrite(D3,HIGH);
           digitalWrite(A3,HIGH);
-          digitalWrite(A1,HIGH);
+          digitalWrite(D1,HIGH);
           digitalWrite(A4,HIGH);
-          digitalWrite(A0,HIGH);
+          digitalWrite(D0,HIGH);
           break;
         case 6:
           digitalWrite(D3,HIGH);
           digitalWrite(A3,HIGH);
-          digitalWrite(A1,HIGH);
+          digitalWrite(D1,HIGH);
           digitalWrite(A4,HIGH);
-          digitalWrite(A0,HIGH);
+          digitalWrite(D0,HIGH);
           digitalWrite(A5,HIGH);
           break;
         case 7:
           digitalWrite(A3,HIGH);
-          digitalWrite(A2,HIGH);
-          digitalWrite(A1,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(D1,HIGH);
           break;
         case 8:
           digitalWrite(D3,HIGH);
-          digitalWrite(A0,HIGH);
-          digitalWrite(A1,HIGH);
-          digitalWrite(A2,HIGH);
+          digitalWrite(D0,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D2,HIGH);
           digitalWrite(A3,HIGH);
           digitalWrite(A4,HIGH);
           digitalWrite(A5,HIGH);
           break;
         case 9:
           digitalWrite(D3,HIGH);
-          digitalWrite(A0,HIGH);
-          digitalWrite(A1,HIGH);
-          digitalWrite(A2,HIGH);
+          digitalWrite(D0,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D2,HIGH);
           digitalWrite(A3,HIGH);
           digitalWrite(A4,HIGH);
           break;
@@ -272,54 +272,54 @@ void firstDigit(int number) {
 void secondDigit(int number) {
     switch(number) {
         case 0:
-            digitalWrite(D0,HIGH);
-            digitalWrite(D1,HIGH);
-            digitalWrite(D2,HIGH);
+            digitalWrite(A0,HIGH);
+            digitalWrite(A1,HIGH);
+            digitalWrite(A2,HIGH);
             digitalWrite(D4,HIGH);
             digitalWrite(D5,HIGH);
             digitalWrite(D6,HIGH);
             break;
         case 1:
-            digitalWrite(D2,HIGH);
+            digitalWrite(A2,HIGH);
             digitalWrite(D4,HIGH);
             break;
         case 2:
             digitalWrite(D7,HIGH);//G
             digitalWrite(D5,HIGH);//A
             digitalWrite(D4,HIGH);//B
-            digitalWrite(D1,HIGH);//D
-            digitalWrite(D0,HIGH);//E
+            digitalWrite(A1,HIGH);//D
+            digitalWrite(A0,HIGH);//E
             break;
         case 3:
             digitalWrite(D7,HIGH);//G
-            digitalWrite(D2,HIGH);//C
+            digitalWrite(A2,HIGH);//C
             digitalWrite(D5,HIGH);//A
             digitalWrite(D4,HIGH);//B
-            digitalWrite(D1,HIGH);//D
+            digitalWrite(A1,HIGH);//D
             break;
         case 4:
             digitalWrite(D7,HIGH);//G
-            digitalWrite(D2,HIGH);//C
+            digitalWrite(A2,HIGH);//C
             digitalWrite(D4,HIGH);//B
             digitalWrite(D6,HIGH);//F
             break;
         case 5:
             digitalWrite(D7,HIGH);//G
-            digitalWrite(D2,HIGH);//C
+            digitalWrite(A2,HIGH);//C
             digitalWrite(D5,HIGH);//A
-            digitalWrite(D1,HIGH);//D
+            digitalWrite(A1,HIGH);//D
             digitalWrite(D6,HIGH);//F
             break;
         case 6:
             digitalWrite(D7,HIGH);//G
-            digitalWrite(D2,HIGH);//C
+            digitalWrite(A2,HIGH);//C
             digitalWrite(D5,HIGH);//A
-            digitalWrite(D1,HIGH);//D
+            digitalWrite(A1,HIGH);//D
             digitalWrite(D6,HIGH);//F
-            digitalWrite(D0,HIGH);//E
+            digitalWrite(A0,HIGH);//E
             break;
         case 7:
-            digitalWrite(D2,HIGH);//C
+            digitalWrite(A2,HIGH);//C
             digitalWrite(D4,HIGH);//B
             digitalWrite(D5,HIGH);//A
             break;
@@ -328,17 +328,17 @@ void secondDigit(int number) {
             digitalWrite(D6,HIGH);//F
             digitalWrite(D5,HIGH);//A
             digitalWrite(D4,HIGH);//B
-            digitalWrite(D2,HIGH);//C
-            digitalWrite(D1,HIGH);//D
-            digitalWrite(D0,HIGH);//E
+            digitalWrite(A2,HIGH);//C
+            digitalWrite(A1,HIGH);//D
+            digitalWrite(A0,HIGH);//E
             break;
         case 9:
             digitalWrite(D7,HIGH);//G
             digitalWrite(D6,HIGH);//F
             digitalWrite(D5,HIGH);//A
             digitalWrite(D4,HIGH);//B
-            digitalWrite(D2,HIGH);//C
-            digitalWrite(D1,HIGH);//D
+            digitalWrite(A2,HIGH);//C
+            digitalWrite(A1,HIGH);//D
             break;
     }
 }
--- display-test/testingdisplay.ino
+++ display-test/testingdisplay.ino
@@ -1,31 +1,48 @@
 int counter = 0;
 
 void setup() {
-    pinMode(D0, OUTPUT);
-    pinMode(D1, OUTPUT);
-    pinMode(D2, OUTPUT);
-    pinMode(D3, OUTPUT);
-    pinMode(D4, OUTPUT);
-    pinMode(D5, OUTPUT);
-    pinMode(D6, OUTPUT);
-    pinMode(D7, OUTPUT);
-    pinMode(A0, OUTPUT);
-    pinMode(A1, OUTPUT);
-    pinMode(A2, OUTPUT);
-    pinMode(A3, OUTPUT);
-    pinMode(A4, OUTPUT);
-    pinMode(A5, OUTPUT);
+    setupDisplay();
 }
 
 void loop() {
-    shownumber(counter);
+    showNumber(counter);
     counter++;
     delay(1000);
+    if(counter == 105) {
+      counter = 0;
+    }
 }
 
-void shownumber(int number) {
+// DISPLAY FUNCTIONS
+
+/**
+* Setup display pins
+*/
+void setupDisplay() {
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+}
+
+/**
+* Show number on the display
+* If the number has more digits than display, show error
+* @param int number - Number to be shown
+*/
+void showNumber(int number) {
     resetDisplay();
-    if(number < 100) {
+    if(number >= 0 && number < 100) {
         int first = number % 10;
         int second = number / 10;
         firstDigit(first);
@@ -35,173 +52,185 @@ void shownumber(int number) {
     }
 }
 
-void firstDigit(int number) {
-    switch(number) {
-        case 0:
-        digitalWrite(A5,HIGH);
-        digitalWrite(A4,HIGH);
-        digitalWrite(A3,HIGH);
-        digitalWrite(A2,HIGH);
-        digitalWrite(A1,HIGH);
-        digitalWrite(A0,HIGH);
-        break;
-        case 1:
-        digitalWrite(A3,HIGH);
-        digitalWrite(A2,HIGH);
-        break;
-        case 2:
-        digitalWrite(D3,HIGH);
-        digitalWrite(A1,HIGH);
-        digitalWrite(A2,HIGH);
-        digitalWrite(A4,HIGH);
-        digitalWrite(A5,HIGH);
-        break;
-        case 3:
-        digitalWrite(D3,HIGH);
-        digitalWrite(A3,HIGH);
-        digitalWrite(A1,HIGH);
-        digitalWrite(A2,HIGH);
-        digitalWrite(A4,HIGH);
-        break;
-        case 4:
-        digitalWrite(D3,HIGH);
-        digitalWrite(A3,HIGH);
-        digitalWrite(A2,HIGH);
-        digitalWrite(A0,HIGH);
-        break;
-        case 5:
-        digitalWrite(D3,HIGH);
-        digitalWrite(A3,HIGH);
-        digitalWrite(A1,HIGH);
-        digitalWrite(A4,HIGH);
-        digitalWrite(A0,HIGH);
-        break;
-        case 6:
-        digitalWrite(D3,HIGH);
-        digitalWrite(A3,HIGH);
-        digitalWrite(A1,HIGH);
-        digitalWrite(A4,HIGH);
-        digitalWrite(A0,HIGH);
-        digitalWrite(A5,HIGH);
-        break;
-        case 7:
-        digitalWrite(A3,HIGH);
-        digitalWrite(A2,HIGH);
-        digitalWrite(A1,HIGH);
-        break;
-        case 8:
-        digitalWrite(D3,HIGH);
-        digitalWrite(A0,HIGH);
-        digitalWrite(A1,HIGH);
-        digitalWrite(A2,HIGH);
-        digitalWrite(A3,HIGH);
-        digitalWrite(A4,HIGH);
-        digitalWrite(A5,HIGH);
-        break;
-        case 9:
-        digitalWrite(D3,HIGH);
-        digitalWrite(A0,HIGH);
-        digitalWrite(A1,HIGH);
-        digitalWrite(A2,HIGH);
-        digitalWrite(A3,HIGH);
-        digitalWrite(A4,HIGH);
-        break;
-    }
-}
-
-void secondDigit(int number) {
-    switch(number) {
-        case 0:
-        digitalWrite(D0,HIGH);
-        digitalWrite(D1,HIGH);
-        digitalWrite(D2,HIGH);
-        digitalWrite(D4,HIGH);
-        digitalWrite(D5,HIGH);
-        digitalWrite(D6,HIGH);
-        break;
-        case 1:
-        digitalWrite(D2,HIGH);
-        digitalWrite(D4,HIGH);
-        break;
-        case 2:
-        digitalWrite(D7,HIGH);//G
-        digitalWrite(D5,HIGH);//A
-        digitalWrite(D4,HIGH);//B
-        digitalWrite(D1,HIGH);//D
-        digitalWrite(D0,HIGH);//E
-        break;
-        case 3:
-        digitalWrite(D7,HIGH);//G
-        digitalWrite(D2,HIGH);//C
-        digitalWrite(D5,HIGH);//A
-        digitalWrite(D4,HIGH);//B
-        digitalWrite(D1,HIGH);//D
-        break;
-        case 4:
-        digitalWrite(D7,HIGH);//G
-        digitalWrite(D2,HIGH);//C
-        digitalWrite(D4,HIGH);//B
-        digitalWrite(D6,HIGH);//F
-        break;
-        case 5:
-        digitalWrite(D7,HIGH);//G
-        digitalWrite(D2,HIGH);//C
-        digitalWrite(D5,HIGH);//A
-        digitalWrite(D1,HIGH);//D
-        digitalWrite(D6,HIGH);//F
-        break;
-        case 6:
-        digitalWrite(D7,HIGH);//G
-        digitalWrite(D2,HIGH);//C
-        digitalWrite(D5,HIGH);//A
-        digitalWrite(D1,HIGH);//D
-        digitalWrite(D6,HIGH);//F
-        digitalWrite(D0,HIGH);//E
-        break;
-        case 7:
-        digitalWrite(D2,HIGH);//C
-        digitalWrite(D4,HIGH);//B
-        digitalWrite(D5,HIGH);//A
-        break;
-        case 8:
-        digitalWrite(D7,HIGH);//G
-        digitalWrite(D6,HIGH);//F
-        digitalWrite(D5,HIGH);//A
-        digitalWrite(D4,HIGH);//B
-        digitalWrite(D2,HIGH);//C
-        digitalWrite(D1,HIGH);//D
-        digitalWrite(D0,HIGH);//E
-        break;
-        case 9:
-        digitalWrite(D7,HIGH);//G
-        digitalWrite(D6,HIGH);//F
-        digitalWrite(D5,HIGH);//A
-        digitalWrite(D4,HIGH);//B
-        digitalWrite(D2,HIGH);//C
-        digitalWrite(D1,HIGH);//D
-        break;
-    }
-}
-
+/**
+* Show minus minus
+*/
 void showError() {
-    // show minus minus
+    resetDisplay();
     digitalWrite(D7,HIGH);
     digitalWrite(D3,HIGH);
 }
 
+/**
+* Switch off display
+*/
 void resetDisplay(){
-    digitalWrite(D0,LOW);
-    digitalWrite(D1,LOW);
-    digitalWrite(D2,LOW);
+    digitalWrite(A0,LOW);
+    digitalWrite(A1,LOW);
+    digitalWrite(A2,LOW);
     digitalWrite(D3,LOW);
     digitalWrite(D4,LOW);
     digitalWrite(D5,LOW);
     digitalWrite(D6,LOW);
     digitalWrite(D7,LOW);
-    digitalWrite(A0,LOW);
-    digitalWrite(A1,LOW);
-    digitalWrite(A2,LOW);
+    digitalWrite(D0,LOW);
+    digitalWrite(D1,LOW);
+    digitalWrite(D2,LOW);
     digitalWrite(A3,LOW);
     digitalWrite(A4,LOW);
     digitalWrite(A5,LOW);
+}
+
+/**
+* Mapping of integer to first segment display
+*/
+void firstDigit(int number) {
+    switch(number) {
+        case 0:
+          digitalWrite(A5,HIGH);
+          digitalWrite(A4,HIGH);
+          digitalWrite(A3,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D0,HIGH);
+          break;
+        case 1:
+          digitalWrite(A3,HIGH);
+          digitalWrite(D2,HIGH);
+          break;
+        case 2:
+          digitalWrite(D3,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(A4,HIGH);
+          digitalWrite(A5,HIGH);
+          break;
+        case 3:
+          digitalWrite(D3,HIGH);
+          digitalWrite(A3,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(A4,HIGH);
+          break;
+        case 4:
+          digitalWrite(D3,HIGH);
+          digitalWrite(A3,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(D0,HIGH);
+          break;
+        case 5:
+          digitalWrite(D3,HIGH);
+          digitalWrite(A3,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(A4,HIGH);
+          digitalWrite(D0,HIGH);
+          break;
+        case 6:
+          digitalWrite(D3,HIGH);
+          digitalWrite(A3,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(A4,HIGH);
+          digitalWrite(D0,HIGH);
+          digitalWrite(A5,HIGH);
+          break;
+        case 7:
+          digitalWrite(A3,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(D1,HIGH);
+          break;
+        case 8:
+          digitalWrite(D3,HIGH);
+          digitalWrite(D0,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(A3,HIGH);
+          digitalWrite(A4,HIGH);
+          digitalWrite(A5,HIGH);
+          break;
+        case 9:
+          digitalWrite(D3,HIGH);
+          digitalWrite(D0,HIGH);
+          digitalWrite(D1,HIGH);
+          digitalWrite(D2,HIGH);
+          digitalWrite(A3,HIGH);
+          digitalWrite(A4,HIGH);
+          break;
+    }
+}
+
+/**
+* Mapping of integer to second segment display
+*/
+void secondDigit(int number) {
+    switch(number) {
+        case 0:
+            digitalWrite(A0,HIGH);
+            digitalWrite(A1,HIGH);
+            digitalWrite(A2,HIGH);
+            digitalWrite(D4,HIGH);
+            digitalWrite(D5,HIGH);
+            digitalWrite(D6,HIGH);
+            break;
+        case 1:
+            digitalWrite(A2,HIGH);
+            digitalWrite(D4,HIGH);
+            break;
+        case 2:
+            digitalWrite(D7,HIGH);//G
+            digitalWrite(D5,HIGH);//A
+            digitalWrite(D4,HIGH);//B
+            digitalWrite(A1,HIGH);//D
+            digitalWrite(A0,HIGH);//E
+            break;
+        case 3:
+            digitalWrite(D7,HIGH);//G
+            digitalWrite(A2,HIGH);//C
+            digitalWrite(D5,HIGH);//A
+            digitalWrite(D4,HIGH);//B
+            digitalWrite(A1,HIGH);//D
+            break;
+        case 4:
+            digitalWrite(D7,HIGH);//G
+            digitalWrite(A2,HIGH);//C
+            digitalWrite(D4,HIGH);//B
+            digitalWrite(D6,HIGH);//F
+            break;
+        case 5:
+            digitalWrite(D7,HIGH);//G
+            digitalWrite(A2,HIGH);//C
+            digitalWrite(D5,HIGH);//A
+            digitalWrite(A1,HIGH);//D
+            digitalWrite(D6,HIGH);//F
+            break;
+        case 6:
+            digitalWrite(D7,HIGH);//G
+            digitalWrite(A2,HIGH);//C
+            digitalWrite(D5,HIGH);//A
+            digitalWrite(A1,HIGH);//D
+            digitalWrite(D6,HIGH);//F
+            digitalWrite(A0,HIGH);//E
+            break;
+        case 7:
+            digitalWrite(A2,HIGH);//C
+            digitalWrite(D4,HIGH);//B
+            digitalWrite(D5,HIGH);//A
+            break;
+        case 8:
+            digitalWrite(D7,HIGH);//G
+            digitalWrite(D6,HIGH);//F
+            digitalWrite(D5,HIGH);//A
+            digitalWrite(D4,HIGH);//B
+            digitalWrite(A2,HIGH);//C
+            digitalWrite(A1,HIGH);//D
+            digitalWrite(A0,HIGH);//E
+            break;
+        case 9:
+            digitalWrite(D7,HIGH);//G
+            digitalWrite(D6,HIGH);//F
+            digitalWrite(D5,HIGH);//A
+            digitalWrite(D4,HIGH);//B
+            digitalWrite(A2,HIGH);//C
+            digitalWrite(A1,HIGH);//D
+            break;
+    }
 }
